2be56c46048d93921263c0b12b00d55fb486a3c8
Leandro (Andy) Galvez <andy_galvez@yahoo.com>
2024-01-25 00:28:52 +0800

    count aws_api_gateway_domain_name

--- main.tf
+++ main.tf
@@ -238,7 +238,7 @@ resource "aws_api_gateway_base_path_mapping" "main" {
   count = var.fqdn == "" ? 0 : 1
   api_id      = aws_api_gateway_rest_api.main.id
   stage_name  = aws_api_gateway_stage.main.stage_name
-  domain_name = aws_api_gateway_domain_name.main.domain_name
+  domain_name = aws_api_gateway_domain_name.main[0].domain_name
 }
 
 resource "aws_api_gateway_usage_plan" "main" {
