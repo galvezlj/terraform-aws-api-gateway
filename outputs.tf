b124f9709e7a647d46b7300b89bf0c9cbb830f7c
Leandro (Andy) Galvez <andy_galvez@yahoo.com>
2024-01-25 15:32:44 +0800

    output execution_arn

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "api_stage" {
 output "invoke_url" {
   value = replace(aws_api_gateway_deployment.main.invoke_url,"/(https://)|(/)/","")
 }
+
+output "execution_arn" {
+  value = aws_api_gateway_rest_api.main.execution_arn
+}
